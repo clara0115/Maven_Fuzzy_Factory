1fad7f9b2dd0ff83fc7c719d79e001a71a90b31e
Clara Gan <106679460+clara0115@users.noreply.github.com>
2022-06-24 11:56:38 +0800

    Add files via upload

--- Business_patterns_and_seasonality.sql
+++ Business_patterns_and_seasonality.sql
@@ -1,33 +1,75 @@
 USE mavenfuzzyfactory;
 
-#ANALYZING SEASONALITY
+#PRODUCT SALES ANALYSIS
 SELECT
-	WEEK(created_at) AS wk,
-    DATE(created_at) AS dt,
-    WEEKDAY(created_at) AS wkday,
-    HOUR(created_at) AS hr,
-    COUNT(DISTINCT website_session_id) AS sessions
-FROM website_sessions
-GROUP BY 1,2,3,4;
+	primary_product_id,
+    COUNT(order_id) AS orders,
+    SUM(price_usd) AS revenue,
+    SUM(price_usd - cogs_usd) as margin,
+    AVG(price_usd) AS average_order_value
+FROM orders
+GROUP BY 1
+ORDER BY 4 DESC;
 
-#ANALYZING BUSINESS PATTERNS
+#PRODUCT LAUNCH SALES ANALYSIS
 SELECT
-	hr,
-    ROUND(AVG(CASE WHEN wkday = 0 THEN website_sessions ELSE NULL END),1) AS mon,
-    ROUND(AVG(CASE WHEN wkday = 1 THEN website_sessions ELSE NULL END),1) AS tue,
-    ROUND(AVG(CASE WHEN wkday = 2 THEN website_sessions ELSE NULL END),1) AS wed,
-    ROUND(AVG(CASE WHEN wkday = 3 THEN website_sessions ELSE NULL END),1) AS thu,
-    ROUND(AVG(CASE WHEN wkday = 4 THEN website_sessions ELSE NULL END),1) AS fri,
-    ROUND(AVG(CASE WHEN wkday = 5 THEN website_sessions ELSE NULL END),1) AS sat,
-    ROUND(AVG(CASE WHEN wkday = 0 THEN website_sessions ELSE NULL END),1) AS sun
-FROM
-(SELECT
-    DATE(created_at) AS dt,
-    WEEKDAY(created_at) AS wkday,
-    HOUR(created_at) AS hr,
-    COUNT(DISTINCT website_session_id) AS website_sessions
+	YEAR(website_sessioNs.created_at) AS yr,
+    MONTH(website_sessions.created_at) AS mon,
+    COUNT(DISTINCT order_id) AS orders,
+    COUNT(DISTINCT order_id)/COUNT(DISTINCT website_session_id) AS conv_rate,
+    SUM(price_usd)/COUNT(DISTINCT website_session_id) AS rev_per_session,
+    COUNT(DISTINCT CASE WHEN primary_product_id = 1 THEN order_id ELSE NULL END) AS product_one_orders,
+    COUNT(DISTINCT CASE WHEN primary_product_id = 2 THEN order_id ELSE NULL END) AS product_two_orders
 FROM website_sessions
-WHERE created_at BETWEEN '2012-09-15' AND '2012-11-15'
-GROUP BY 1,2,3) AS daily_hourly_sessions
-GROUP BY hr;
+	LEFT JOIN orders USING(website_session_id)
+WHERE website_sessions.created_at BETWEEN '2012-04-01' AND '2013-04-05'
+GROUP BY 1,2;
+
+#CROSS SELL ANALYSIS
+SELECT
+	primary_product_id,
+    product_id AS cross_sold_product_id,
+    COUNT(DISTINCT orders.order_id) AS orders
+FROM orders
+	LEFT JOIN order_items 
+		ON orders.order_id = order_items.order_id
+		AND is_primary_item = 0
+GROUP BY 1,2
+ORDER BY 3 DESC;
+
+#CONVERSION FUNNEL FOR DIFFERENT PRODUCTS
+CREATE TEMPORARY TABLE tbl2 
+SELECT
+	website_session_id, 
+    MAX(fuzzy_page) as fuzzy_made_it, 
+    MAX(bear_page) as bear_made_it, 
+    MAX(cart_page) AS cart_made_it, 
+    MAX(shipping_page) AS shipping_made_it, 
+    MAX(billing_page) AS billing_made_it, 
+    MAX(thankyou_page) AS thankyou_made_it 
+FROM (
+SELECT website_sessions.website_session_id,
+	CASE WHEN pageview_url='/the-original-mr-fuzzy' THEN 1 ELSE 0 END AS fuzzy_page, 
+	CASE WHEN pageview_url='/the-forever-love-bear' THEN 1 ELSE 0 END AS bear_page, 
+	CASE WHEN pageview_url = '/cart' THEN 1 ELSE 0 END AS cart_page, 
+	CASE WHEN pageview_url = '/shipping' THEN 1 ELSE 0 END AS shipping_page, 
+	CASE WHEN pageview_url = '/billing-2' THEN 1 ELSE 0 END AS billing_page,
+	CASE WHEN pageview_url = '/thank-you-for-your-order' THEN 1 ELSE 0 END AS thankyou_page 
+FROM website_sessions 
+	LEFT JOIN website_pageviews USING (website_session_id) 
+WHERE website_sessions.created_at BETWEEN '2013-01-06' and '2013-04-10'
+	AND pageview_url IN ('/the-original-mr-fuzzy','/the-forever-love-bear','/cart','/shipping','/billing-2','/thank-you-for-your-order')) as table1 GROUP BY 1;
     
+SELECT 
+	CASE 
+    WHEN fuzzy_made_it=1 THEN 'mrfuzzy' 
+	WHEN bear_made_it=1 THEN 'lovebear' 
+    ELSE NULL END AS product_seen, 
+    COUNT(DISTINCT website_session_id) AS sessions, 
+    COUNT(DISTINCT CASE WHEN cart_made_it = 1 THEN website_session_id ELSE NULL END) AS to_cart,
+	COUNT(DISTINCT CASE WHEN shipping_made_it = 1 THEN website_session_id ELSE NULL END) AS to_shipping, 
+	COUNT(DISTINCT CASE WHEN billing_made_it = 1 THEN website_session_id ELSE NULL END) AS to_billing,
+	COUNT(DISTINCT CASE WHEN thankyou_made_it = 1 THEN website_session_id ELSE NULL END) AS to_thankyou 
+FROM tbl2
+GROUP BY 1;
+
